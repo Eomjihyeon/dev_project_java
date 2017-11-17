da8a34c7417f42940087ddf5581fdbfde37f2864
sjw7870 <sjw4890@gmail.com>
2017-11-17 17:19:37 +0900

    Jquery

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 "# dev_education" 
 - git add .
 - git commit -m "당일 작업 이름"
-- git push guest 
+- git push guest
---
+++ javaScript/1117/js/js.js
@@ -0,0 +1,8 @@
+
+    $(document).ready(function() {
+        $('h1').css('background','pink');
+    }
+    );
+
+//$선택자 제이쿼리로 요소를 선택 h1요소를  jquery객체로 가져오는것.
+// parent > element 자식인 elment 요소 선택 자손택자
