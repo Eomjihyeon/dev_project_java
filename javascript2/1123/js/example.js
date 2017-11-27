7dc8e1c19d9cd2ba7aad682c6a49ec4bb92a69dd
wlgus20208 <wlgus20208@naver.com>
2017-11-27 17:33:57 +0900

    animate

---
+++ javascript2/1123/js/example.js
@@ -0,0 +1,23 @@
+
+var year = 2017;
+function display(birth) {
+    alert((year-birth)+"살입니다") ;
+    }
+
+  var Register = function (name,birthYear,job,address,postcod,date) {
+  this.name=name;
+  this.birthYear=birthYear;
+  this.job=job;
+  this.address=address;
+  this.postcode=postcode;
+  this.date=date;
+  }
+
+
+  text += "<tr>";
+  text += "<th>순위</th>";
+  text += "<th></th>";
+  text += "<th></th>";
+  text += "<th></th>";
+  text += "<th></th>";
+  text += "<th></th>";
