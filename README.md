261be85da0304bd853a9022f3bdd35c6c0e7a1b3
wlgus20208 <wlgus20208@naver.com>
2017-11-28 17:32:45 +0900

    onepage

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 - cd 마우스로 드래그 위치소스
 - git add .
 - git commit -m "당일 작업 이름"
-- git push guest
+        - git push guest
--- javascript2/1127/js/index.js
+++ javascript2/1127/js/index.js
@@ -1,6 +1,43 @@
-
+/*===============================================================
+           wow
+==================================================================*/
 
 $(function() {
-  // Handler for .ready() called.
+new WOW().init();
+
 });
 
+/*===============================================================
+        Maginfic popUp(http://dimsemenov.com/plugins/magnific-popup/documentation.html)
+==================================================================*/
+$(function() {
+$('#work').magnificPopup({
+  delegate: 'a', // child items selector, by clicking on it popup will open
+  type: 'image',
+  // other options
+  gallery: {
+  enabled:true
+  }
+});
+
+});
+
+/*
+$('.gallery').each(function() { //뉴스처럼됨
+    $(this).magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    gallery: {
+    enabled:true
+    }
+});
+});*/
+
+/*===============================================================
+        owl car
+==================================================================*/
+$(function() {
+  $(".owl-carousel").owlCarousel({
+  items : 3,
+ });
+});
