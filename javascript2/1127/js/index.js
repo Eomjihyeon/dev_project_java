7c17dc3821938a026b180a81ef5df28da65c002c
wlgus20208 <wlgus20208@naver.com>
2017-11-29 17:27:16 +0900

    randingpage

--- javascript2/1127/js/index.js
+++ javascript2/1127/js/index.js
@@ -37,7 +37,51 @@ $('.gallery').each(function() { //뉴스처럼됨
         owl car
 ==================================================================*/
 $(function() {
-  $(".owl-carousel").owlCarousel({
+  $("#team-members").owlCarousel({
   items : 3,
+  autoplay:true,
+  smartSpeed:700,
+  loop:true,
+  autoplayHoverPause:true
+ });
+});
+
+
+/*===============================================================
+      test
+==================================================================*/
+$(function() {
+  $("#customers-testimonials").owlCarousel({
+  items : 1,
+  autoplay:true,
+  smartSpeed:700,
+  loop:true,
+  autoplayHoverPause:true
+ });
+});
+
+/*===============================================================
+        counterup
+   https://github.com/ciromattia/jquery.counterup
+==================================================================*/
+$(function() {
+  $('.counter').counterUp({
+    delay :10,
+    time: 2000
+  });
+
+});
+
+/*===============================================================
+        counterup
+   https://github.com/ciromattia/jquery.counterup
+==================================================================*/
+$(function() {
+  $("#clients-list").owlCarousel({
+  items : 3,
+  autoplay:true,
+  smartSpeed:700,
+  loop:true,
+  autoplayHoverPause:true
  });
 });
